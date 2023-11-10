227f98f6d6b59f39c1f8f163ce906734079ad31c
MiracleWong <cfwr1991@126.com>
2023-11-10 16:42:44 +0800

    20231110 Add Zap: SugaredLogger

--- zap/main.go
+++ zap/main.go
@@ -7,31 +7,29 @@ import (
 )
 
 var logger *zap.Logger
+var sugarLogger *zap.SugaredLogger
 
 func main() {
 	InitLogger()
-	defer logger.Sync()
+	defer sugarLogger.Sync()
 	simpleHttpGet("www.baidu.com")
 	simpleHttpGet("http://www.baidu.com")
 }
 
 func InitLogger() {
 	logger, _ = zap.NewProduction()
+	sugarLogger = logger.Sugar()
 }
 
 func simpleHttpGet(url string) {
+	sugarLogger.Debugf("Trying to hit GET request for %s", url)
 	resp, err := http.Get(url)
 	if err != nil {
 		// 日志记录器的语法
 		// func (log *Logger) MethodXXX(msg string, fields ...Field)
-		logger.Error(
-			"Error fetching url..",
-			zap.String("url", url),
-			zap.Error(err))
+		sugarLogger.Errorf("Error fetching URL %s : Error = %s", url, err)
 	} else {
-		logger.Info("Success..",
-			zap.String("statusCode", resp.Status),
-			zap.String("url", url))
+		sugarLogger.Infof("Success! statusCode = %s for URL %s", resp.Status, url)
 		resp.Body.Close()
 	}
 }
